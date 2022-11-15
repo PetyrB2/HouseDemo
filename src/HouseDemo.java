1b272477b891c55db7edce1a8a7c1d3767630626
Peter Broadbent <103116958+PetyrB2@users.noreply.github.com>
2022-11-15 14:27:45 +0000

    Initial Commit

---
+++ src/HouseDemo.java
@@ -0,0 +1,18 @@
+
+public class HouseDemo {
+
+	public static void main(String[] args) {
+		// Intro
+		System.out.println("=======================================================\n");
+		System.out.println("Title: House Demo");
+		System.out.println("Genre: Java Basics");
+		System.out.println("Author: Peter Broadbent");
+		System.out.println("Language: Java \n" + "Version: " + System.getProperty("java.version"));
+		System.out.println("Description:\n\tA basic Java program to list houses and "
+				+ "\n\ttheir details, such as number of windows etc.");
+		System.out.println("=======================================================\n");
+		System.out.println("");
+
+	}
+
+}
