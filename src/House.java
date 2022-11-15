c0b26fbe3cc09e07afa19390e704fb253597286c
Peter Broadbent <103116958+PetyrB2@users.noreply.github.com>
2022-11-15 15:36:35 +0000

    Refactored  Added a printHouse method and have added parameterised constructors to make adding a new house esier and quicker

--- src/House.java
+++ src/House.java
@@ -1,4 +1,96 @@
 
 public class House {
 
-}
+	// Attributes
+	private String ID;
+	private int houseNumber;
+	private String address;
+	private int numStories;
+	private int numBedRooms;
+	private int numWindows;
+	private String colour;
+
+	// No-Arg Constructor (Default ctor)
+	public House() {
+		ID = "Z001";
+		houseNumber = 1;
+		address = "Home";
+		numStories = 2;
+		numBedRooms = 2;
+		numWindows = 8;
+		colour = "White";
+	}// end ctor
+
+	// Parameterised Constructor
+	public House(String ID, int houseNumber, String address, int numStories, 
+			int numBedRooms, int numWindows, String colour) {
+
+		this.ID = ID;
+		this.houseNumber = houseNumber;
+		this.address = address;
+		this.numStories = numStories;
+		this.numBedRooms = numBedRooms;
+		this.numWindows = numWindows;
+		this.colour = colour;
+		
+	}// end ctor
+
+	// Getters and Setters
+
+	public String getID() {
+		return ID;
+	}
+
+	public void setID(String iD) {
+		ID = iD;
+	}
+
+	public int getHouseNumber() {
+		return houseNumber;
+	}
+
+	public void setHouseNumber(int houseNumber) {
+		this.houseNumber = houseNumber;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public int getNumStories() {
+		return numStories;
+	}
+
+	public void setNumStories(int numStories) {
+		this.numStories = numStories;
+	}
+
+	public int getNumWindows() {
+		return numWindows;
+	}
+
+	public void setNumWindows(int numWindows) {
+		this.numWindows = numWindows;
+	}
+
+	public String getColour() {
+		return colour;
+	}
+
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+
+	public int getNumBedRooms() {
+		return numBedRooms;
+	}
+
+	public void setNumBedRooms(int numBedRooms) {
+		this.numBedRooms = numBedRooms;
+	}
+
+}// EOF
