25738f0709b8140a5b48751a853f17512da55cab
Peter Broadbent <103116958+PetyrB2@users.noreply.github.com>
2022-11-15 15:01:16 +0000

    Added code for Listings and made the display to be  acceptable

--- src/House.java
+++ src/House.java
@@ -1,4 +1,71 @@
 
 public class House {
 
-}
+	// Attributes
+	private String ID;
+	private int houseNumber;
+	private String address;
+	private int numStories;
+	private int numBedRooms;
+	private int numWindows;
+	private String colour;
+
+	// Getters and Setters
+
+	public String getID() {
+		return ID;
+	}
+
+	public void setID(String iD) {
+		ID = iD;
+	}
+
+	public int getHouseNumber() {
+		return houseNumber;
+	}
+
+	public void setHouseNumber(int houseNumber) {
+		this.houseNumber = houseNumber;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public int getNumStories() {
+		return numStories;
+	}
+
+	public void setNumStories(int numStories) {
+		this.numStories = numStories;
+	}
+
+	public int getNumWindows() {
+		return numWindows;
+	}
+
+	public void setNumWindows(int numWindows) {
+		this.numWindows = numWindows;
+	}
+
+	public String getColour() {
+		return colour;
+	}
+
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+
+	public int getNumBedRooms() {
+		return numBedRooms;
+	}
+
+	public void setNumBedRooms(int numBedRooms) {
+		this.numBedRooms = numBedRooms;
+	}
+
+}// EOF
--- src/HouseDemo.java
+++ src/HouseDemo.java
@@ -3,7 +3,7 @@ public class HouseDemo {
 
 	public static void main(String[] args) {
 		// Intro
-		System.out.println("=======================================================\n");
+		System.out.println("=======================================================");
 		System.out.println("Title: House Demo");
 		System.out.println("Genre: Java Basics");
 		System.out.println("Author: Peter Broadbent");
@@ -11,8 +11,53 @@ public class HouseDemo {
 		System.out.println("Description:\n\tA basic Java program to list houses and "
 				+ "\n\ttheir details, such as number of windows etc.");
 		System.out.println("=======================================================\n");
-		System.out.println("");
 
+		// Begin code
+		House theGateHouse47 = new House();
+		House theTowerHouse = new House();
+
+		theGateHouse47.setID("OX001");
+		theGateHouse47.setNumBedRooms(4);
+		theGateHouse47.setAddress("The Gate House, Oxshott");
+		theGateHouse47.setColour("White");
+		theGateHouse47.setHouseNumber(47);
+		theGateHouse47.setNumStories(3);
+		theGateHouse47.setNumWindows(14);
+
+		theTowerHouse.setID("LON001");
+		theTowerHouse.setHouseNumber(1);
+		theTowerHouse.setAddress("The Tower, London");
+		theTowerHouse.setNumStories(4);
+		theTowerHouse.setNumBedRooms(6);
+		theTowerHouse.setNumWindows(12);
+		theTowerHouse.setColour("Grey");
+
+		System.out.println("Begin Listings....");
+		System.out.println("");
+		
+		// Print out the Gate House 47
+		System.out.println("=================================================");
+		System.out.println("Listing ID: " + theGateHouse47.getID());
+		System.out.println("House Number: " + theGateHouse47.getHouseNumber());
+		System.out.println("House Address: " + theGateHouse47.getAddress());
+		System.out.println("Number of Stories: " + theGateHouse47.getNumStories());
+		System.out.println("Number of Bedrooms: " + theGateHouse47.getNumBedRooms());
+		System.out.println("Number of Windows: " + theGateHouse47.getNumWindows());
+		System.out.println("Colour: " + theGateHouse47.getColour());
+		System.out.println("");
+		System.out.println("=================================================");
+		System.out.println("");
+		
+		// Print out the Tower House
+		System.out.println("Listing ID: " + theTowerHouse.getID());
+		System.out.println("House Number: " + theTowerHouse.getHouseNumber());
+		System.out.println("House Address: " + theTowerHouse.getAddress());
+		System.out.println("Number of Stories: " + theTowerHouse.getNumStories());
+		System.out.println("Number of Bedrooms: " + theTowerHouse.getNumBedRooms());
+		System.out.println("Number of Windows: " + theTowerHouse.getNumWindows());
+		System.out.println("Colour: " + theTowerHouse.getColour());
+		System.out.println("");
+		System.out.println("=================================================");
 	}
 
 }
